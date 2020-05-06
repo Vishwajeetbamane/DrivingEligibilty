8e5037d9559d06b5b42cfd4b8daf4d792fd57031
Vishwajeet bamane <56468218+Vishwajeetbamane@users.noreply.github.com>
2020-05-06 13:05:28 +0530

    Add files via upload
    Program for checking if you are eligible for driving or not.

---
+++ Eligibility_for_driving.py
@@ -0,0 +1,10 @@
+
+print("Please enter your age")
+var1 = int(input())
+var2 = 18
+if var1<var2:
+	print("Sorry,You are not eligible for driving.")
+if var1>120:
+	print("Sorry, Human lifespan exceeded.\n Enter proper age.")
+else:
+	print("You are eligible for Driving.")
